0adfacf02af8eebfacf0e345cf8f11c9f38aaa21
ianthony4 <achaisa@unsa.edu.pe>
2023-10-15 22:51:46 -0500

    Subiendo proyecto

---
+++ Pruebita1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebita1;
+
+/**
+ *
+ * @author USER
+ */
+public class Pruebita1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nepe de chiki");
+    }
+    
+}
